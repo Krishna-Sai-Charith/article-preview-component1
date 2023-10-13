9132192d3e7fc899e2f671cfc2a683725fd49e44
Krishna Sai Charith <137757171+Krishna-Sai-Charith@users.noreply.github.com>
2023-10-13 07:03:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function visibilty(){
+    var speechBubble=document.getElementById("speech-bubble");
+    if(speechBubble.style.visibility==="hidden"){
+        speechBubble.style.visibility="visible";
+    }
+    else{
+        speechBubble.style.visibility="hidden";
+    }
+
+}
